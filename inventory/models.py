7ee205911b81f580f24097396835b5e52b11fe2a
uh-karsi <karisannk@nycstudents.net>
2023-07-06 09:04:43 -0400

    got the data

--- inventory/models.py
+++ inventory/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
-
-# Create your models here.
-
+categories = [('HARDWARE','Hardware'),('ART SUPPLIES','Art Supplies'),('OTHER','Other')]
 class Item(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, editable=True)
     name = models.CharField(max_length=100, blank=True, default='')
     description = models.TextField()
     quantity = models.IntegerField(default=0)
     is_available = models.BooleanField(default=True)
+    category = models.CharField(max_length=50, choices=categories, default='')
+
+    def __str__(self):
+        return self.name
+
+
+
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import mixins, generics
+from django.http import JsonResponse
+from .models import Item
+from .serializer import ItemSerializer
 
-# Create your views here.
+class ItemsView(generics.GenericAPIView, mixins.ListModelMixin):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
