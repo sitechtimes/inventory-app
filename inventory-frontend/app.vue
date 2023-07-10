4c3c1844b7aaeb801b4fcaf42cff4888892e2b22
Aiden8641 <90789887+Aiden8641@users.noreply.github.com>
2023-07-10 08:39:47 -0400

    idk some user icon menu thing

--- inventory-frontend/app.vue
+++ inventory-frontend/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <User />
   </div>
 </template>
---
+++ inventory-frontend/components/User.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <button @click="click"></button>
+    <div class="content" v-if="show">
+      <NuxtLink class="link"> Settings </NuxtLink>
+      <NuxtLink class="link"> items </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let show = ref(false);
+
+const click = () => {
+  if (show.value) {
+    show.value = false;
+  } else {
+    show.value = true;
+  }
+};
+</script>
+
+<style scoped>
+button {
+  border-radius: 2rem;
+  background-color: gray;
+  width: 3rem;
+  height: 3rem;
+  border: none;
+}
+button:hover {
+  cursor: pointer;
+}
+.content {
+  padding: 10px;
+  background-color: gray;
+  width: 5rem;
+  border-radius: 10px;
+}
+.link {
+  margin-top: 10rem;
+}
+</style>
