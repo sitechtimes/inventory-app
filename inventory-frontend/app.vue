9fd21e97e8df4a79925a6bd6b987cefbcadd091c
Aiden8641 <90789887+Aiden8641@users.noreply.github.com>
2023-07-13 11:41:22 -0400

    creating component to move quantities

--- inventory-frontend/app.vue
+++ inventory-frontend/app.vue
@@ -1,40 +1,9 @@
 <template>
   <div>
-    wow?
-    <div id="itemHolder">
-      <Item
-        :name="'Hammers Huge Huge hammer'"
-        :quantity="2"
-        :description="'This is a trial. These hammers are awesome and slay and cool and I love hammers.  '"
-        :available="true"
-        :updated="'2023-7-10'"
-      />
-    </div>
+    <NuxtPage />
   </div>
 </template>
 
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300&display=swap");
+<style></style>
 
-body {
-  background-color: #ebe3e2;
-  color: #ffffff;
-  font-family: "Fira Sans", sans-serif;
-}
-#itemHolder {
-  display: flex;
-  height: 100%;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-}
-</style>
-
-<script>
-import Item from "./components/item.vue";
-
-export default {
-  name: "app",
-  components: { Item },
-};
-</script>
+<script></script>
---
+++ inventory-frontend/components/MoveItems.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="change">
+    <div class="location">
+      <h2>Makerspace</h2>
+      <h2>Back Room</h2>
+    </div>
+    <div class="amount">
+      <h3>{{ Makerspace }}</h3>
+      <h3>{{ Backroom }}</h3>
+    </div>
+    <div v-if="haveSupplies" class="error">
+      <h2>None left in {{ room }}</h2>
+    </div>
+    <div class="btn">
+      <button @click="toMakerspace"></button>
+      <button @click="toBackRoom"></button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+let Makerspace = ref(2);
+let Backroom = ref(0);
+let haveSupplies = ref(false);
+let room = ref("");
+
+function toBackRoom() {
+  if (Makerspace.value > 0) {
+    room.value = "";
+    haveSupplies.value = false;
+    Backroom.value += 1;
+    Makerspace.value -= 1;
+  } else {
+    room.value = "Makerspace";
+    haveSupplies.value = true;
+  }
+}
+
+function toMakerspace() {
+  if (Backroom.value > 0) {
+    room.value = "";
+    haveSupplies.value = false;
+
+    Makerspace.value += 1;
+    Backroom.value -= 1;
+  } else {
+    room.value = "Backroom";
+    haveSupplies.value = true;
+  }
+}
+</script>
+
+<style scoped>
+.change {
+  border: solid 1px black;
+  width: 20rem;
+  height: 10rem;
+  display: flex;
+  flex-flow: column nowrap;
+}
+.location,
+.amount,
+.btn {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.amount {
+  height: 3rem;
+}
+button {
+  border: none;
+  width: 5rem;
+  height: 2.5rem;
+}
+</style>
