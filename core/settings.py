9d41b983859e0aaa0020b2df6fef2725c9d5beab
Henry Zheng <zhenghenry2@gmail.com>
2023-07-17 18:01:41 -0400

    remove duplicate package

--- core/settings.py
+++ core/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'inventory',
-    'django_extensions',
-    'corsheaders'
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
