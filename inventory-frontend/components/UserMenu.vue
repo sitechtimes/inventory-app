50e756964ab9d34ed32134e95a4116f52303903c
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-07-31 08:03:34 -0400

    dfbgnb

--- inventory-frontend/components/UserMenu.vue
+++ inventory-frontend/components/UserMenu.vue
@@ -6,6 +6,49 @@
           <font-awesome-icon :icon="['fas', 'bars']" class="open-menu-icon" />
         </div>
       </button>
+      <div class="mobile-menu dismiss">
+        <div class="mobile-user">
+          <div class="user-icon-cont">
+            <font-awesome-icon
+              :icon="['fas', 'user']"
+              class="mobile-user-icon"
+            />
+          </div>
+          <div class="mobile-user-info">
+            <h2 class="mobile-user-title">My Account</h2>
+            <h3 class="mobile-username">user1234@gmail.com</h3>
+          </div>
+        </div>
+        <div class="mobile-menu-container">
+          <button class="mobile-menu-btn">
+            <div>
+              <font-awesome-icon
+                :icon="['fas', 'boxes-stacked']"
+                class="mobile-icon"
+              />
+            </div>
+            <h3 class="mobile-tag">CATALOG</h3>
+          </button>
+          <button class="mobile-menu-btn">
+            <div>
+              <font-awesome-icon
+                :icon="['fas', 'hand-holding-hand']"
+                class="mobile-icon"
+              />
+            </div>
+            <h3 class="mobile-tag">BORROWED</h3>
+          </button>
+          <button class="mobile-menu-btn">
+            <div>
+              <font-awesome-icon
+                :icon="['fas', 'circle-exclamation']"
+                class="mobile-icon"
+              />
+            </div>
+            <h3 class="mobile-tag">SUPPLIES NEEDED</h3>
+          </button>
+        </div>
+      </div>
     </div>
     <div class="return-box" @click="NavMenu">
       <div></div>
@@ -68,25 +111,27 @@ export default {
   },
   methods: {
     NavMenu() {
-      if (this.NavCont.classList.contains("selected")) {
-        this.NavCont.classList.remove("selected");
-        this.NavCont.classList.add("dismiss");
+      const MobileMenu = document.querySelector(".mobile-menu");
+      // const returnbox = document.querySelector(".return-box");
+      if (MobileMenu.classList.contains("selected")) {
+        MobileMenu.classList.remove("selected");
+        MobileMenu.classList.add("dismiss");
         console.log("dismiss");
-        this.returnbox.classList.remove("return-box-display");
-        this.returnbox.classList.add("return-box-nodisplay");
-      } else if (this.NavCont.classList.contains("dismiss")) {
-        this.NavCont.classList.remove("dismiss");
-        this.NavCont.classList.add("selected");
+        // returnbox.classList.remove("return-box-display");
+        // returnbox.classList.add("return-box-nodisplay");
+      } else if (MobileMenu.classList.contains("dismiss")) {
+        MobileMenu.classList.remove("dismiss");
+        MobileMenu.classList.add("selected");
         console.log("selected");
-        this.returnbox.classList.remove("return-box-nodisplay");
-        this.returnbox.classList.add("return-box-display");
+        // returnbox.classList.remove("return-box-nodisplay");
+        // returnbox.classList.add("return-box-display");
       } else if (
-        !this.NavCont.classList.contains("dismiss") ||
-        !this.NavCont.classList.contains("selected")
+        !MobileMenu.classList.contains("dismiss") ||
+        !MobileMenu.classList.contains("selected")
       ) {
-        this.NavCont.classList.add("selected");
+        MobileMenu.classList.add("selected");
         console.log("selected");
-        this.returnbox.classList.add("return-box-display");
+        // returnbox.classList.add("return-box-display");
       }
     },
     runOnMounted() {
@@ -115,11 +160,9 @@ export default {
   flex-flow: row;
 }
 .return-box-display {
-  display: display;
-
   height: 100vh;
   width: 100vw;
-  opacity: 0.2;
+  opacity: 1;
 }
 .return-box-nodisplay {
   display: none;
@@ -187,7 +230,7 @@ export default {
 }
 .menu-btn {
   padding: 1rem;
-  width: 100%;
+  width: 38.5%;
   border: none;
 
   color: white;
@@ -197,6 +240,11 @@ export default {
   justify-content: space-evenly;
   align-items: center;
   transition: background-color 0.2s linear;
+}
+h3 {
+  color: black;
+  padding-left: 0.5rem;
+  font-size: 0.6rem;
 }
 .menu-btn:hover {
   background-color: var(--lightblue);
@@ -212,6 +260,8 @@ export default {
   display: none;
   width: 2rem;
   height: 2rem;
+  top: 1%;
+  position: fixed;
 }
 .open-menu-btn {
   width: 100%;
@@ -226,8 +276,61 @@ export default {
   height: 100%;
 }
 .open-menu-icon {
+  width: 200%;
+  height: 200%;
+}
+.mobile-menu {
+  display: grid;
+  grid-template-columns: 4rem 1fr;
+  grid-template-rows: 4rem 1fr;
+  padding-top: 5vh;
+  flex-direction: column;
+  position: relative;
+  z-index: 1;
+  background-color: white;
+  width: 100vw;
+  height: 100vh;
+}
+.mobile-menu-btn {
+  margin-top: 3rem;
   width: 100%;
-  height: 100%;
+  height: 10vh;
+  text-decoration: none;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.mobile-user-icon,
+.mobile-icon {
+  color: #c7d6d5;
+  width: 4rem;
+  height: 4rem;
+}
+.mobile-user-icon {
+  color: black;
+}
+.mobile-user-title,
+.mobile-username,
+.mobile-tag {
+  color: black;
+  font-size: 4rem;
+}
+.mobile-user {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+.mobile-user-title,
+.mobile-username {
+  font-size: 2rem;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+.mobile-menu-btn:hover {
+  background-color: var(--lightblue);
 }
 /* animations  */
 .selected {
@@ -241,20 +344,45 @@ export default {
 }
 @keyframes slide-out {
   0% {
+    -webkit-transform: translateX(0%);
     transform: translateX(0%);
   }
   100% {
-    transform: translateX(-100%);
+    -webkit-transform: translateX(-500%);
+    transform: translateX(-500%);
+  }
+}
+@-webkit-keyframes slide-out {
+  0% {
+    -webkit-transform: translateX(0%);
+    transform: translateX(0%);
+  }
+  100% {
+    -webkit-transform: translateX(-500%);
+    transform: translateX(-500%);
   }
 }
 @keyframes slide-in {
   0% {
-    -webkit-transform: translateX(-100%);
+    -webkit-transform: translateX(-500%);
+    transform: translateX(-500%);
   }
   100% {
     -webkit-transform: translateX(0%);
+    transform: translateX(0%);
   }
 }
+@-webkit-keyframes slide-in {
+  0% {
+    -webkit-transform: translateX(-500%);
+    transform: translateX(-500%);
+  }
+  100% {
+    -webkit-transform: translateX(0%);
+    transform: translateX(0%);
+  }
+}
+
 /* media query */
 /* mobile */
 @media (max-width: 1200px) {
