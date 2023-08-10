ec4675f0cdbe776aac60c72b65646219162b7b22
Aiden8641 <90789887+Aiden8641@users.noreply.github.com>
2023-08-10 08:25:40 -0400

    Revert "get sql working"
    This reverts commit f677eaa64ce4e3edc9c745af498c131bf0d6c823.

--- core/settings.py
+++ core/settings.py
@@ -85,18 +85,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': '<sakil_full>',
-        'USER': '<root>',
-        'PASSWORD': 'q1w2e3!@#',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
-""" 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    } """
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
