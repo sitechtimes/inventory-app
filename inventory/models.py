b0e15094047d5605f284f1a3a904636184d0cfac
richie <113203265+R1chi333@users.noreply.github.com>
2023-09-28 10:26:53 -0400

    .

--- inventory/models.py
+++ inventory/models.py
@@ -8,25 +8,6 @@ vendors = [('DOE', 'ShopDOE'), ('AMZ', 'Amazon'),
            ('BLICK', 'Blick'), ('HD', 'Home Depot')]
 locations = [('MS', 'Makerspace'), ('BR', 'Back Room')]
 
-class Log(models.Model):
-    item_id = models.CharField(max_length=100, blank=True, default='')
-    name = models.CharField(max_length=100, blank=True, default='')
-    purchase_link = models.CharField(max_length=1000, blank=True, default='')
-    image_url = models.URLField(blank=True, null=True, default=None)
-    image_file = models.ImageField(
-        upload_to=upload_to, blank=True, null=True, default=None)
-    last_purchased = models.DateTimeField(auto_now=True, editable=True)
-    backroom_quantity = models.IntegerField(default=0)
-    makerspace_quantity = models.IntegerField(default=0)
-    category = models.ForeignKey(
-        Category, related_name='itemsCategory', on_delete=models.CASCADE)
-    vendor = models.ForeignKey(
-        Vendor, related_name='itemsVendor', on_delete=models.CASCADE)
-    location = models.CharField(max_length=50, default='')
-    min_amount = models.IntegerField(default=5)
-    pub_date = models.DateField()
-    
-
 class Category(models.Model):
     category_code = models.CharField(
         choices=categories, max_length=100, default="")
@@ -79,19 +60,4 @@ class Item(models.Model):
         self.id = count_obj
         super(Item, self).save(*args, **kwargs)
 
-class Reporter(models.Model):
-    full_name = models.CharField(max_length=70)
-
-    def __str__(self):
-        return self.full_name
-
-
-class Article(models.Model):
-    pub_date = models.DateField()
-    headline = models.CharField(max_length=200)
-    content = models.TextField()
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.headline
 # image = models.CharField(max_length=1000, blank=True, default='')
