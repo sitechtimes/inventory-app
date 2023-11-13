a6a498b836ea72850a76bbe6768841a60fa1e2ef
LeoDwi <leod13579@gmail.com>
2023-11-13 14:52:15 -0500

    Revert "Update header.vue"
    This reverts commit ab491b7ce0f6b131cf592bc8b3b0713edff3c7ab.

--- inventory-frontend/components/header.vue
+++ inventory-frontend/components/header.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="longrow">
-    <!--     <link
+    <link
       rel="stylesheet"
       href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-    /> -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
-      rel="stylesheet"
     />
     <div class="fixedleft">
       <div class="fromnav">
@@ -113,6 +107,7 @@ export default {
 </script>
 
 <style>
+
 .addItem,
 .notif {
   height: 4rem;
