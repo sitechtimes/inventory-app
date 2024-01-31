b7cf4445a463689d9785d6528f9a34a1eccf9a1a
richie <113203265+R1chi333@users.noreply.github.com>
2024-01-31 11:06:49 -0500

    .

--- inventory-frontend/cypress/e2e/spec.cy.js
+++ inventory-frontend/cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('/')
+    cy.get('.addItem > .material-symbols-outlined').click()
+    cy.get('#item_id').type('tester123ID{enter}')
+    cy.get('#name').type('tester123Name{enter}')
+    cy.get('#quantity-makerspace').type('abc123{enter}')
+    cy.get('#quantity-backroom').type('456{enter')
   })
 })
